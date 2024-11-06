6420981975506189b338a999c52a4b4e5a391067
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-11-06 15:42:29 -0300

    dbname = if0_37666554_devlobcypresstests

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $servername = "sql308.infinityfree.com";
 $username = "if0_37666554";
 $password = "h9e8s3w2";
-$dbname = "if0_37666554_XXX"; // Substitua "XXX" pelo nome correto do seu banco
+$dbname = "if0_37666554_devlobcypresstests";
 
 // Criando a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
