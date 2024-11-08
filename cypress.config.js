8be0b66c449bb1167103b10e2e142e5b39a1bd24
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-11-08 17:16:22 -0300

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
 
   e2e: {
     video: true,
-    baseUrl: 'http://localhost/',
+    baseUrl: 'http://localhost:3306',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
