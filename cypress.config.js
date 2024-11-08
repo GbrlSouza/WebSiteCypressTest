f3746cf375be76388c3058e763cb1e353b0f5997
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-11-08 16:48:56 -0300

    add github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+
+    mochaJunitReporterReporterOptions: { mochaFile: 'cypress/reports/junit/results-[hash].xml' },
+
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
+  },
+
+  chromeWebSecurity: false,
+
   e2e: {
     video: true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
